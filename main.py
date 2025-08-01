4d1a4286a984f8dd94cefeb5d7ef7e118d190ac4
Yuvraj Singh <149161662+yuvraj-singh009@users.noreply.github.com>
2025-08-01 19:14:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from modules import dns_lookup, whois_info, port_scan, ssl_info, vuln_scan
+# from modules import dns_lookup, whois_info, port_scan, ssl_info, vuln_scan
 
 def run_scan(target):
     os.makedirs("output", exist_ok=True)
